d8016e671da59044e58246c4112d0575aa75a08d
Gregor Burger <burger.gregor@gmail.com>
2013-01-15 17:22:11 +0100

    add query test

--- main.cpp
+++ main.cpp
@@ -25,15 +25,29 @@ void gen_random(std::vector<vector> &points) {
     });
 }
 
+void query_test(std::vector<vector> &points, SpatialHash<vector> &hash) {
+#pragma omp parallel for
+    for (size_t i = 0; i < points.size() / 3; i++) {
+        std::vector<vector> results = hash.query(points[i]);
+        //qDebug() << "queried" << results.size() << "results";
+    }
+}
+
+#ifdef NOGUI
+#define LEN 10000000
+#else
+#define LEN 1000
+#endif
+
 int main(int argc, char **argv) {
-    std::vector<vector> points(100000000);
+    std::vector<vector> points(LEN);
     gen_random(points);
 
-    SpatialHash<vector> hash(0.05);
-
+    SpatialHash<vector> hash(10000.0f/LEN);
     TIME_IT(hash.insert(points));
-
-#ifndef NOGUI
+#ifdef NOGUI
+    TIME_IT(query_test(points, hash));
+#else
     QApplication a(argc, argv);
     MainWindow w(&hash);
     w.show();
